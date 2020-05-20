8ca8c85298790ad625fc0585e339c7d83496db36
Paul Han <paul.myung.han@gmail.com>
2020-05-20 11:34:08 -0700

    changed workout tracker picture

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use(express.json());
 app.use(routes);
 require('./services/passport');
 // Connect database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/reduxTodo', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chat', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.listen(PORT);
---
+++ notes.md
@@ -0,0 +1,1 @@
+go through project 2 and go through console.logs
