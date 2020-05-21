ce6a05adcc12709250dda2b6136fcd0542103078
Paul Han <paul.myung.han@gmail.com>
2020-05-21 13:43:53 -0700

    types correction

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,6 @@
+//counter section
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
-
-
 
 // Todos Section
 export const GET_ALL_TODOS = 'GET_ALL_TODOS';
@@ -12,6 +11,13 @@ export const GET_ALL_TODOS_ERROR = 'GET_ALL_TODOS_ERROR';
 export const GET_USER_TODOS = 'GET_USER_TODOS';
 export const GET_USER_TODOS_ERROR = 'GET_USER_TODOS_ERROR';
 
+// Add user todos section
+export const ADD_TODO = 'ADD_TODO';
+export const ADD_TODO_ERROR = 'ADD_TODO_ERROR';
+
+
+// Update User Todo by ID
+export const UPDATE_TODO_BY_ID_ERROR = 'UPDATE_TODO_BY_ID_ERROR';
 
 //  User authentication
 export const AUTH_USER = 'AUTH_USER';
