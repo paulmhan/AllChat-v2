bba41bb43c57922089eea4a63b85eef1540662be
Paul Han <paul.myung.han@gmail.com>
2020-05-27 14:33:31 -0700

    fixed route not working

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -11,9 +11,9 @@ import SignIn from '../SignIn';
 
 import { connect } from 'react-redux';
 import Navbar from './../../components/Navbar';
-import io from "socket.io-client";
+// import io from "socket.io-client";
 
-const socket = io();
+// const socket = io();
 
 class App extends Component {
   render () {
@@ -21,11 +21,17 @@ class App extends Component {
       <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 700 }}>
           <Navbar isLoggedIn={this.props.authenticated}/>
-          <Route exact path='/signin'  render={() => <SignIn socket={socket} />}/>
+          {/* <Route exact path='/signin'  render={() => <SignIn socket={socket} />}/>
           <Route exact path='/signup'  render={() => <SignUp socket={socket} />}/>
           <Route exact path='/signout'  render={() => <SignOut socket={socket} />}/>
           <Route exact path='/chat'  render={() => <Chat socket={socket} />}/>
           <Route exact path='/'  render={() => <LandingPage socket={socket} />}/>
+           */}
+          <Route exact path='/signin' component={SignIn}/>
+          <Route exact path='/signup' component={SignUp}/>
+          <Route exact path='/signout' component={SignOut}/>
+          <Route exact path='/chat' component={Chat}/>
+          <Route exact path='/' component={LandingPage}/>
         </Grid.Column>
       </Grid>
     );
