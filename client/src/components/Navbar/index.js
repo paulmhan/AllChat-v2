f9d7e86bc02c8ae8ebea7fec324ab76261ddfee9
James Geib <jpgeib2@gmail.com>
2020-06-03 19:20:43 -0700

    half figured out a solution for the navbar render, have a strong lead for a solution

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,19 +4,54 @@ import { Link } from 'react-router-dom';
 import { Menu, Button } from 'semantic-ui-react';
 import "./style.css";
 
-
-
-export default (props) => (
-  <Menu secondary widths={12} id="navbar">
-    <Menu.Item id="allchat-icon" position="left">
-      <AllChatTitle />
-    </Menu.Item>
-    { props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?"/> }
-    { props.isLoggedIn 
-    ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout"/> 
-    : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
-  </Menu>
-);
-
+export default (props) => {
+  const page = window.location.pathname;
+  console.log(page);
+  
+  if(page === "/") {
+    return (
+        <Menu secondary widths={12} id="navbar">
+          <Menu.Item id="allchat-icon" position="left">
+            <AllChatTitle />
+          </Menu.Item>
+          {props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
+          {props.isLoggedIn
+            ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+            : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+        </Menu>
+      );
+  } else if(page === "/signup" || page === "/signin" || page === "/signout") {
+    return (
+        <Menu secondary widths={12} id="navbar">
+          <Menu.Item id="allchat-icon" position="left">
+            <AllChatTitle />
+          </Menu.Item>
+        </Menu>
+      );
+  } else if(page === "/rooms" || page === "/chat") {
+    return (
+      <Menu secondary widths={12} id="navbar">
+        <Menu.Item id="allchat-icon" position="left">
+          <AllChatTitle />
+        </Menu.Item>
+        {props.isLoggedIn
+          ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+          : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+      </Menu>
+    );
+  }
+}
 
 // { props.isLoggedIn ? null : <Menu.Item as={Link} to='/' content='Sign Up'/> }
+
+// (
+//   <Menu secondary widths={12} id="navbar">
+//     <Menu.Item id="allchat-icon" position="left">
+//       <AllChatTitle />
+//     </Menu.Item>
+//     {props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
+//     {props.isLoggedIn
+//       ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+//       : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+//   </Menu>
+// );
