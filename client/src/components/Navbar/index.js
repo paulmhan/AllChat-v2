206f362be253567bd7eff2ec32fab86426aba243
James Geib <jpgeib2@gmail.com>
2020-06-04 12:27:08 -0700

    close to figuring out navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -26,45 +26,86 @@ class Navbar extends Component {
       case page === "/chat":
         return this.setState({ currentPage: "/chat" });
       default:
-        return this.setState({ currentPage: "/" });
+        return this.state;
     }
   }
 
   render() {
-    if (page === "/") {
-      console.log(page, "Mount");
-      return (
-        <Menu secondary widths={12} id="navbar">
-          <Menu.Item id="allchat-icon" position="left">
-            <AllChatTitle />
-          </Menu.Item>
-          {props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
-          {props.isLoggedIn
-            ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
-            : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
-        </Menu>
-      );
-    } else if (page === "/signup" || page === "/signin" || page === "/signout") {
-      console.log(page, "Mount 2");
-      return (
-        <Menu secondary widths={12} id="navbar">
-          <Menu.Item id="allchat-icon" position="left">
-            <AllChatTitle />
-          </Menu.Item>
-        </Menu>
-      );
-    } else if (page === "/rooms" || page === "/chat") {
-      console.log(page, "Mount 3")
-      return (
-        <Menu secondary widths={12} id="navbar">
-          <Menu.Item id="allchat-icon" position="left">
-            <AllChatTitle />
-          </Menu.Item>
-          {props.isLoggedIn
-            ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
-            : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
-        </Menu>
-      );
+    switch (this.state) {
+      case this.state.currentPage === "/":
+        return (
+          <Menu secondary widths={12} id="navbar">
+            <Menu.Item id="allchat-icon" position="left">
+              <AllChatTitle />
+            </Menu.Item>
+            {this.props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
+            {this.props.isLoggedIn
+              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+          </Menu>
+        );
+      case this.state.currentPage === "/signup":
+        return (
+          <Menu secondary widths={12} id="navbar">
+            <Menu.Item id="allchat-icon" position="left">
+              <AllChatTitle />
+            </Menu.Item>
+            {this.props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
+            {this.props.isLoggedIn
+              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+          </Menu>
+        );
+      case this.state.currentPage === "/signin":
+        return (
+          <Menu secondary widths={12} id="navbar">
+            <Menu.Item id="allchat-icon" position="left">
+              <AllChatTitle />
+            </Menu.Item>
+          </Menu>
+        );
+      case this.state.currentPage === "/signout":
+        return (
+          <Menu secondary widths={12} id="navbar">
+            <Menu.Item id="allchat-icon" position="left">
+              <AllChatTitle />
+            </Menu.Item>
+          </Menu>
+        );
+      case this.state.currentPage === "/chat":
+        return (
+          <Menu secondary widths={12} id="navbar">
+            <Menu.Item id="allchat-icon" position="left">
+              <AllChatTitle />
+            </Menu.Item>
+            {this.props.isLoggedIn
+              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+          </Menu>
+        );
+      case this.state.currentPage === "/rooms":
+        return (
+          <Menu secondary widths={12} id="navbar">
+            <Menu.Item id="allchat-icon" position="left">
+              <AllChatTitle />
+            </Menu.Item>
+            {this.props.isLoggedIn
+              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+          </Menu>
+        );
+      default:
+        return (
+          <Menu secondary widths={12} id="navbar">
+            <Menu.Item id="allchat-icon" position="left">
+              <AllChatTitle />
+            </Menu.Item>
+            {this.props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
+            {this.props.isLoggedIn
+              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+          </Menu>
+        );
     }
   }
 }
