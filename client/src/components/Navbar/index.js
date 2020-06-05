4126c0c185052c30dc85535de2abf0305efc93b1
James Geib <jpgeib2@gmail.com>
2020-06-05 11:52:50 -0700

    fooled around with Navbar idea, half worked, giving up again

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,117 +7,102 @@ import "./style.css";
 class Navbar extends Component {
 
   state = {
-    currentPage: ""
+    currentPage: window.location.pathname
   }
 
   handleCurrentPage = () => {
-    const page = window.location.pathname;
-    if(page === "/") {
-      return this.setState({ currentPage: "/" });
-    } else if(page === "/signup") {
-      return this.setState({ currentPage: "/signup" });
-    } else if(page === "/signin") {
-      return this.setState({ currentPage: "/signin" });
-    } else if( page === "/signout") {
-      return this.setState({ currentPage: "/signout" });
-    } else if(page === "/rooms") {
-      return this.setState({ currentPage: "/rooms" });
-    } else if(page === "/chat") {
-      return this.setState({ currentPage: "/chat" });
-    }   
-  }
-  
-  render() {
-    console.log("Current Page: ", this.state.currentPage );
-    switch (this.state) {
-      case this.state.currentPage === "/":
-        return (
-          <Menu secondary widths={12} id="navbar">
-            <Menu.Item id="allchat-icon" position="left">
-              <AllChatTitle />
-            </Menu.Item>
-            {this.props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
-            {this.props.isLoggedIn
-              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
-              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
-          </Menu>
-        );
-      case this.state.currentPage === "/signup":
-        return (
-          <Menu secondary widths={12} id="navbar">
-            <Menu.Item id="allchat-icon" position="left">
-              <AllChatTitle />
-            </Menu.Item>
-            {this.props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
-            {this.props.isLoggedIn
-              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
-              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
-          </Menu>
-        );
-      case this.state.currentPage === "/signin":
-        return (
-          <Menu secondary widths={12} id="navbar">
-            <Menu.Item id="allchat-icon" position="left">
-              <AllChatTitle />
-            </Menu.Item>
-          </Menu>
-        );
-      case this.state.currentPage === "/signout":
-        return (
-          <Menu secondary widths={12} id="navbar">
-            <Menu.Item id="allchat-icon" position="left">
-              <AllChatTitle />
-            </Menu.Item>
-          </Menu>
-        );
-      case this.state.currentPage === "/chat":
-        return (
-          <Menu secondary widths={12} id="navbar">
-            <Menu.Item id="allchat-icon" position="left">
-              <AllChatTitle />
-            </Menu.Item>
-            {this.props.isLoggedIn
-              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
-              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
-          </Menu>
-        );
-      case this.state.currentPage === "/rooms":
-        return (
-          <Menu secondary widths={12} id="navbar">
-            <Menu.Item id="allchat-icon" position="left">
-              <AllChatTitle />
-            </Menu.Item>
-            {this.props.isLoggedIn
-              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
-              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
-          </Menu>
-        );
-      default:
-        return(
-          <Menu secondary widths={12} id="navbar">
-            <Menu.Item id="allchat-icon" position="left">
-              <AllChatTitle />
-            </Menu.Item>
-            {this.props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
-            {this.props.isLoggedIn
-              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
-              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
-          </Menu>
-        );
+    if (this.state.currentPage === "/") {
+      this.setState({ currentPage: "/" });
+    } else if (this.state.currentPage === "/signup") {
+      this.setState({ currentPage: "/signup" });
+    } else if (this.state.currentPage === "/signin") {
+      this.setState({ currentPage: "/signin" });
+    } else if (this.state.currentPage === "/signout") {
+      this.setState({ currentPage: "/signout" });
+    } else if (this.state.currentPage === "/rooms") {
+      this.setState({ currentPage: "/rooms" });
+    } else if (this.state.currentPage === "/chat") {
+      this.setState({ currentPage: "/chat" });
     }
+  }
+
+  handleNavbarRender = () => {
+    if (this.state.currentPage === "/") {
+      return (
+        <>
+          <Menu.Item id="allchat-icon" position="left">
+            <AllChatTitle />
+          </Menu.Item>
+          {this.props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
+          {
+            this.props.isLoggedIn
+              ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+              : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>
+          }
+        </>
+      );
+    } else if (this.state.currentPage === "/signup") {
+      return (
+        <>
+          <Menu.Item id="allchat-icon" position="left">
+            <AllChatTitle />
+          </Menu.Item>
+          {this.props.isLoggedIn ? <Menu.Item as={Link} to="/rooms" position="right" content="Chatrooms" id="room-select" /> : <Menu.Item position="right" content="Already a user?" />}
+          {this.props.isLoggedIn
+            ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+            : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+        </>
+      );
+    } else if (this.state.currentPage === "/signin") {
+      return (
+        <>
+          <Menu.Item id="allchat-icon" position="left">
+            <AllChatTitle />
+          </Menu.Item>
+        </>
+      );
+    } else if (this.state.currentPage === "/signout") {
+      return (
+        <>
+          <Menu.Item id="allchat-icon" position="left">
+            <AllChatTitle />
+          </Menu.Item>
+        </>
+      );
+    } else if (this.state.currentPage === "/rooms") {
+      return (
+        <>
+          <Menu.Item id="allchat-icon" position="left">
+            <AllChatTitle />
+          </Menu.Item>
+          {this.props.isLoggedIn
+            ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+            : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+        </>
+      )
+    } else if (this.state.currentPage === "/chat") {
+      return (
+        <>
+          <Menu.Item id="allchat-icon" position="left">
+            <AllChatTitle />
+          </Menu.Item>
+          {this.props.isLoggedIn
+            ? <Menu.Item as={Link} to='/signout' position="right" content='Sign Out' id="signout" />
+            : <Menu.Item><Button as={Link} to='/signin' id="signin">Sign In</Button></Menu.Item>}
+        </>
+      )
+    }
+  }
+
+  render() {
+    console.log("Current Page: ", this.state.currentPage);
+    return (
+      <Menu secondary widths={12} id="navbar">
+        { this.handleNavbarRender() }
+      </Menu>
+    );
   }
 }
 
+
 export default Navbar;
-
-
-
-
-
-// (props) => {
-
-//   const page = window.location.pathname;
-
-//   
-//   }
-// }
