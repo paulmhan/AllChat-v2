000bfa7b391e8bcfd0635e51cc2805595c28c0a5
James Geib <jpgeib2@gmail.com>
2020-06-07 13:03:37 -0700

    Working on responsive layout

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Button } from "semantic-ui-react";
+import { Responsive, Grid, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 import "./style.css";
@@ -9,41 +9,189 @@ class LandingPage extends Component {
 
     render() {
         return (
-
-            <Grid id="landing-container">
-                <Grid.Column width={8}>
-                    <Grid container id="inner-landing-container">
-                        <Grid.Row centered>
+        <>
+            <Responsive
+                {...Responsive.onlyMobile}
+                {...Responsive.onlyTablet}
+                as={Grid}
+                id="landing-container">
+                <Responsive
+                    {...Responsive.onlyMobile}
+                    {...Responsive.onlyTablet}
+                    as={Grid.Column}
+                    width={8}>
+                    <Responsive
+                        {...Responsive.onlyMobile}
+                        {...Responsive.onlyTablet}
+                        as={Grid} container
+                        id="inner-landing-container">
+                        <Responsive
+                            {...Responsive.onlyMobile}
+                            {...Responsive.onlyTablet}
+                            as={Grid.Row}
+                            centered>
                             <h1 id="welcome">Welcome to AllChat!</h1>
-                        </Grid.Row>
-                        <Grid.Row centered>
-                            <Grid.Column id="header" width={16}>
+                        </Responsive>
+                        <Responsive
+                            {...Responsive.onlyMobile}
+                            {...Responsive.onlyTablet}
+                            as={Grid.Row}
+                            centered>
+                            <Responsive
+                                {...Responsive.onlyMobile}
+                                {...Responsive.onlyTablet}
+                                as={Grid.Column}
+                                id="header" width={16}>
                                 <h1 id="together">Bring People Together</h1>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row centered>
-                            <Grid.Column id="text1" width={16}>
+                            </Responsive>
+                        </Responsive>
+                        <Responsive
+                            {...Responsive.onlyMobile}
+                            {...Responsive.onlyTablet}
+                            as={Grid.Row}
+                            centered>
+                            <Responsive {...Responsive.onlyMobile} {...Responsive.onlyTablet} as={Grid.Column} id="text1" width={16}>
                                 <h5 id="paragraph">As communication continues to increase on a global scale, so too does the demand for quick, reliable translation.
                                 With AllChat, you can talk to anyone across the world, even if you don't speak their language!
                                 </h5>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row centered>
-                            <Grid.Column width={4}>
+                            </Responsive>
+                        </Responsive>
+                        <Responsive
+                            {...Responsive.onlyMobile}
+                            {...Responsive.onlyTablet}
+                            as={Grid.Row}
+                            centered>
+                            <Responsive
+                                {...Responsive.onlyMobile}
+                                {...Responsive.onlyTablet}
+                                as={Grid.Column}
+                                width={4}>
                                 <Button as={Link} to='/signup' size="massive" id="get-started">Get Started</Button>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Grid.Column>
-                <Grid.Column width={8}>
-                    <Grid container>
-                        <Grid.Row centered>
+                            </Responsive>
+                        </Responsive>
+                    </Responsive>
+                </Responsive>
+                <Responsive
+                    {...Responsive.onlyMobile}
+                    {...Responsive.onlyTablet}
+                    as={Grid.Column}
+                    width={8}>
+                    <Responsive
+                        {...Responsive.onlyMobile}
+                        {...Responsive.onlyTablet}
+                        as={Grid}
+                        container>
+                        <Responsive
+                            {...Responsive.onlyMobile}
+                            {...Responsive.onlyTablet}
+                            as={Grid.Row}
+                            centered>
                             <img id="landing-page-image" alt="people-chatting" src={require("../../assets/images/people-chatting.png")} />
-                        </Grid.Row>
-                    </Grid>
-                </Grid.Column>
-            </Grid>
-
+                        </Responsive>
+                    </Responsive>
+                </Responsive>
+            </Responsive>
+            
+            <Responsive
+                {...Responsive.onlyComputer}
+                {...Responsive.onlyLargeScreen}
+                {...Responsive.onlyWideScreen}
+                as={Grid}
+                id="landing-container">
+                <Responsive
+                    {...Responsive.onlyComputer}
+                    {...Responsive.onlyLargeScreen}
+                    {...Responsive.onlyWideScreen}
+                    as={Grid.Column}
+                    width={8}>
+                    <Responsive
+                        {...Responsive.onlyComputer}
+                        {...Responsive.onlyLargeScreen}
+                        {...Responsive.onlyWideScreen}
+                        as={Grid} container
+                        id="inner-landing-container">
+                        <Responsive
+                            {...Responsive.onlyComputer}
+                            {...Responsive.onlyLargeScreen}
+                            {...Responsive.onlyWideScreen}
+                            as={Grid.Row}
+                            centered>
+                            <h1 id="welcome">Welcome to AllChat!</h1>
+                        </Responsive>
+                        <Responsive
+                            {...Responsive.onlyComputer}
+                            {...Responsive.onlyLargeScreen}
+                            {...Responsive.onlyWideScreen}
+                            as={Grid.Row}
+                            centered>
+                            <Responsive
+                                {...Responsive.onlyComputer}
+                                {...Responsive.onlyLargeScreen}
+                                {...Responsive.onlyWideScreen}
+                                as={Grid.Column}
+                                id="header" width={16}>
+                                <h1 id="together">Bring People Together</h1>
+                            </Responsive>
+                        </Responsive>
+                        <Responsive
+                            {...Responsive.onlyComputer}
+                            {...Responsive.onlyLargeScreen}
+                            {...Responsive.onlyWideScreen}
+                            as={Grid.Row}
+                            centered>
+                            <Responsive
+                                {...Responsive.onlyComputer} 
+                                {...Responsive.onlyLargeScreen} 
+                                {...Responsive.onlyWideScreen}
+                                as={Grid.Column} 
+                                id="text1" 
+                                width={16}>
+                                <h5 id="paragraph">As communication continues to increase on a global scale, so too does the demand for quick, reliable translation.
+                                With AllChat, you can talk to anyone across the world, even if you don't speak their language!
+                                </h5>
+                            </Responsive>
+                        </Responsive>
+                        <Responsive
+                            {...Responsive.onlyComputer}
+                            {...Responsive.onlyLargeScreen}
+                            {...Responsive.onlyWideScreen}
+                            as={Grid.Row}
+                            centered>
+                            <Responsive
+                                {...Responsive.onlyComputer}
+                                {...Responsive.onlyLargeScreen}
+                                {...Responsive.onlyWideScreen}
+                                as={Grid.Column}
+                                width={4}>
+                                <Button as={Link} to='/signup' size="massive" id="get-started">Get Started</Button>
+                            </Responsive>
+                        </Responsive>
+                    </Responsive>
+                </Responsive>
+                <Responsive
+                    {...Responsive.onlyComputer}
+                    {...Responsive.onlyLargeScreen}
+                    {...Responsive.onlyWideScreen}
+                    as={Grid.Column}
+                    width={8}>
+                    <Responsive
+                        {...Responsive.onlyComputer}
+                        {...Responsive.onlyLargeScreen}
+                        {...Responsive.onlyWideScreen}
+                        as={Grid}
+                        container>
+                        <Responsive
+                            {...Responsive.onlyComputer}
+                            {...Responsive.onlyLargeScreen}
+                            {...Responsive.onlyWideScreen}
+                            as={Grid.Row}
+                            centered>
+                            <img id="landing-page-image" alt="people-chatting" src={require("../../assets/images/people-chatting.png")} />
+                        </Responsive>
+                    </Responsive>
+                </Responsive>
+            </Responsive>
+        </>
         )
 
     }
