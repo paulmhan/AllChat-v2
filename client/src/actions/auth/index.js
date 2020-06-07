7a94184540b2cb202cedfac279b60471e5eaf5d4
Paul Han <paul.myung.han@gmail.com>
2020-06-06 18:33:26 -0700

    signout function passed as onclick to navbar directly

--- client/src/actions/auth/index.js
+++ client/src/actions/auth/index.js
@@ -2,16 +2,6 @@ import { AUTH_USER, GET_USER, GET_USER_ERROR, LEAVE_USER } from "../types";
 import axios from "axios";
 
 
-// export const signOut = () => {
-//     localStorage.removeItem("token");
-//     console.log("hit sign out");
-//     // dispatch({type: LEAVE_USER, payload: ""});
-//     return {
-//         type: AUTH_USER, 
-//         payload: ""
-//     };
-// }
-
 
 export const signOut = () => async dispatch => {
   localStorage.removeItem("token");
@@ -23,7 +13,6 @@ export const signOut = () => async dispatch => {
 
 
 export const loadUser = () => async dispatch => {
-  console.log("loadUser")
     if(localStorage.getItem("token")){
       try {
         const { data } = await axios.get("/api/getuser", { headers: { 'authorization': localStorage.getItem('token')}});
