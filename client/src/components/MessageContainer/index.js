a34dac1564207851e59b652959fa59fbb34bb339
James Geib <jpgeib2@gmail.com>
2020-06-09 10:22:04 -0700

    commented out autoscroll code

--- client/src/components/MessageContainer/index.js
+++ client/src/components/MessageContainer/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Message } from "semantic-ui-react";
 import moment from 'moment';
-import ReactAutoScroll from "react-to-target-auto-scroll";
+// import ReactAutoScroll from "react-to-target-auto-scroll";
 import "./style.css";
 
 class MessageContainer extends Component {
@@ -16,15 +16,15 @@ class MessageContainer extends Component {
 
   render() {
     return (
-      <ReactAutoScroll
-        targetPosition={5000}
-        easeType={"linear"}
-        speed={15}
-        updateInterval={1}
-        onScrollingDone={ () => console.log("scrolling finished") }
-        scrollTargetRef={this.refs.autoScroll}
-        isEnabled
-      >
+      // <ReactAutoScroll
+      //   targetPosition={5000}
+      //   easeType={"linear"}
+      //   speed={15}
+      //   updateInterval={1}
+      //   onScrollingDone={ () => console.log("scrolling finished") }
+      //   scrollTargetRef={this.refs.autoScroll}
+      //   isEnabled
+      // >
         <div ref="autoScroll" className="ui message" id="message-container">
           {this.props.messages?.map((message, index) =>
             <Message key={index}>
@@ -34,7 +34,7 @@ class MessageContainer extends Component {
               <Message.Header> <p><small>{message.firstName}&nbsp;{message.lastName}:&nbsp;{message.text}</small></p></Message.Header>
             </Message>)}
         </div>
-      </ReactAutoScroll>
+      // </ReactAutoScroll>
     )
   }
 };
