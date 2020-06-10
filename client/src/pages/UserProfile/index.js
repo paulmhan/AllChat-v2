646df5e09c456b846c1d24374e6576ff4229e5a5
James Geib <jpgeib2@gmail.com>
2020-06-10 10:36:12 -0700

    preparing to build and format preliminary  user profile section

--- client/src/pages/UserProfile/index.js
+++ client/src/pages/UserProfile/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { Grid, Image } from "semantic-ui-react";
 
 class UserProfile extends Component {
     render() {
         return (
             <div>
-                <h1>This is the User Profile page</h1>
+                <Image />
             </div>
         );
 
