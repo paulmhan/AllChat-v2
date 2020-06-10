7aa8588d38c4d4b88cecd3f1cb2b97ab2d61e9ba
James Geib <jpgeib2@gmail.com>
2020-06-10 11:02:04 -0700

    created two folder structures for modals on profile page

--- client/src/pages/UserProfile/index.js
+++ client/src/pages/UserProfile/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import { Grid, Image } from "semantic-ui-react";
+import ProfilePic from "../../assets/images/Tim_Allen_headshot.jpg";
+
+import "./style.css";
 
 class UserProfile extends Component {
     render() {
         return (
-            <div>
-                <Image />
-            </div>
+            <Grid id="profile-container">
+                <Grid.Row centered>
+                    <Image circular src={ProfilePic} />
+                </Grid.Row>
+                <Grid.Row centered>
+                        <h1>Name: Tim Allen</h1>
+                </Grid.Row>
+                <Grid.Row centered>
+                        <h1>Email: home@improvement.com</h1>
+                </Grid.Row>
+            </Grid>
         );
 
     }
