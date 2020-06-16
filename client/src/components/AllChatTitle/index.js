5922a59aa0b3c7517e207791ccd5b2bb7f45f0d6
James Geib <jpgeib2@gmail.com>
2020-06-15 17:42:48 -0700

    added logo to navbar

--- client/src/components/AllChatTitle/index.js
+++ client/src/components/AllChatTitle/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Image } from "semantic-ui-react";
+import AllChatLogo from "../../assets/images/AllChat-Logo.png";
 
 import "./style.css";
 
 const AllChatTitle = props => {
     return (
-        <div id="allchat-icon">
-            <Link to="/" name="home">
-                <h1 id="main-header">AllChat</h1>
-            </Link>
+        <div id="allchat-icon-container">
+            {/* <Link to="/" name="home"> */}
+                {/* <h1 id="main-header">AllChat</h1> */}
+                <Image id="allchat-logo" as={Link} to="/" name="home" fluid src={AllChatLogo} />
+            {/* </Link> */}
         </div>
 
     )
