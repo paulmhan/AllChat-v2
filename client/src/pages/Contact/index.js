d57bcda52991b4a86f553dde2166f2252234eb2e
Paul Han <paul.myung.han@gmail.com>
2020-06-15 19:21:00 -0700

    contact page header now translated to all languages

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { Grid, Header } from "semantic-ui-react";
 import ContactForm from "../../components/ContactForm";
 import emailjs from 'emailjs-com';
+import { connect } from 'react-redux';
+import { compose } from "redux";
+import { loadUser } from "../../actions/auth";
+import content from "../../content.js";
+
 import "./style.css";
 
 class Contact extends Component {
@@ -13,8 +18,44 @@ class Contact extends Component {
     success: ""
   }
 
-  onSubmit = event => {
-    // event.preventDefault();
+  async componentDidMount() {
+    this.props.user || await this.props.loadUser();
+  }
+
+  renderContact(language) {
+    switch (language) {
+      case "es":
+        return content.contact.es;
+      case "zh":
+        return content.contact.zh;
+      case "ar":
+        return content.contact.ar;
+      case "fr":
+        return content.contact.fr;
+      case "de":
+        return content.contact.de;
+      case "hi":
+        return content.contact.hi;
+      case "it":
+        return content.contact.it;
+      case "ja":
+        return content.contact.ja;
+      case "ko":
+        return content.contact.ko;
+      case "ru":
+        return content.contact.ru;
+      case "tl":
+        return content.contact.tl;
+      case "te":
+        return content.contact.te;
+      case "vi":
+        return content.contact.vi;
+      default:
+        return content.contact.en;
+    };
+  }
+
+  onSubmit = () => {
     emailjs.sendForm('default_service', 'template_8ThKxTDq', "#contact-form", "user_UQ5mLuD7ryVQD1fmgdrQX")
       .then(() => {
         this.setState({
@@ -37,18 +78,19 @@ class Contact extends Component {
     return (
       <Grid id="contact-container">
         <Grid.Column width={16}>
-          <Header id="contact-header" as="h1">Contact Us:</Header>
+          <Header id="contact-header" as="h1">{this.renderContact(this.props.user?.language)}</Header>
         </Grid.Column>
         <Grid.Column width={16}>
-          <Header id="contact-par" as="p">We value all and any feedback! Whether you have suggestions on components we can improve on, questions on the functionality of our application, or just want to say hello, our inbox is always open.</Header>
+          <Header id="contact-par" as="p">We value any and all feedback! Whether you have suggestions on components we can improve on, questions on the functionality of our application, or just want to say hello, our inbox is always open.</Header>
         </Grid.Column>
-        <Grid.Column  width={16}>
+        <Grid.Column width={16}>
           <Header id="contact-form-directions" as="h4">Please fill out the following fields:</Header>
         </Grid.Column>
         <Grid.Column width={16}>
           <ContactForm
             onSubmit={this.onSubmit}
             success={this.state.success}
+            use={this.props.user}
           />
         </Grid.Column>
 
@@ -57,5 +99,12 @@ class Contact extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    user: state.auth.currentUser
+  }
+}
 
-export default Contact;
+export default compose(
+  connect(mapStateToProps, { loadUser }),
+)(Contact)
