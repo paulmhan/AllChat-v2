f16b7369a91400388943e1fcdb35d27889116757
Paul Han <paul.myung.han@gmail.com>
2020-06-19 13:33:04 -0700

    deleted all commented out code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require("./routes");
 const roomController = require("./controllers/roomController");
-const userController = require("./controllers/userController");
 const messageController = require("./controllers/messageController");
 const jwt = require("jwt-simple");
 const path = require("path");
@@ -48,7 +47,6 @@ io.on("connection", socket => {
     })
     socket.on("createRoom", data => {
         //data is the room name and userID
-        // socket.join(data.roomName)
         roomController.createRoom(data, newRoom => {
             io.emit("serverToClientRoom", newRoom);
         });
