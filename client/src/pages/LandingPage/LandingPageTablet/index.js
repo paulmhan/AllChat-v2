0fa7fbb2db65edc6e6f7d6071ab9e7f0daedb50d
James Geib <jpgeib2@gmail.com>
2020-06-19 12:56:26 -0700

    LandingPage will now appear on all screen sizes

--- client/src/pages/LandingPage/LandingPageTablet/index.js
+++ client/src/pages/LandingPage/LandingPageTablet/index.js
@@ -315,30 +315,30 @@ class LandingPageTablet extends Component {
     render() {
         return (
             <>
-                <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid} id="landing-container-computer">
-                    <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} width={16}>
-                        <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid} container id="inner-landing-container-computer">
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
+                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} id="landing-container-computer">
+                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} container id="inner-landing-container-computer">
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
                                 <h1 id="welcome-computer">
                                     {this.renderWelcome(this.props.user?.language)}
                                 </h1>
                             </Responsive>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
-                                <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} id="header-computer" width={16}>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
+                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} id="header-computer" width={16}>
                                     <h1 id="together-computer">
                                         {this.renderBring(this.props.user?.language)}
                                     </h1>
                                 </Responsive>
                             </Responsive>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
-                                <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} id="text1-computer" width={16}>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
+                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} id="text1-computer" width={16}>
                                     <h5 id="paragraph-computer">
                                         {this.renderParagraph(this.props.user?.language)}
                                     </h5>
                                 </Responsive>
                             </Responsive>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
-                                <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} width={5}>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
+                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={5}>
                                     {this.props.isLoggedIn ? <Button fluid as={Link} to='/rooms' size="massive" id="get-started-computer">
                                         {this.renderStart(this.props.user?.language)}
                                     </Button> : <Button fluid as={Link} to='/signup' size="massive" id="get-started-computer">Get Started</Button>}
@@ -346,23 +346,23 @@ class LandingPageTablet extends Component {
                             </Responsive>
                         </Responsive>
                     </Responsive>
-                    <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} width={16}>
-                        <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid} container>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
+                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} container>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
                                 <Image fluid id="landing-page-image-computer" alt="people-chatting" src={PeopleChatting} />
                             </Responsive>
                         </Responsive>
                     </Responsive>
                 </Responsive>
 
-                <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid} id="demo-container-computer">
-                    <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} width={16}>
-                        <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid} container>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
+                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} id="demo-container-computer">
+                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} container>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
                                 <Header as="h1" id="demo-header-computer">{this.renderDemo(this.props.user?.language)}</Header>
                             </Responsive>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
-                                <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} width={16}>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
+                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
                                     <Header id="demo-subheader1-computer" as="h4">
                                     {this.renderDemoParagraph(this.props.user?.language)}
                                     </Header>
@@ -373,31 +373,31 @@ class LandingPageTablet extends Component {
                             </Responsive>
                         </Responsive>
                     </Responsive>
-                    <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} width={16}>
-                        <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid} container>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
+                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} container>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
                                 <Image fluid id="chat-gif-computer" alt="chat-gif" src={AllChatDemo1} />
                             </Responsive>
                         </Responsive>
                     </Responsive>
                 </Responsive>
 
-                <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid} id="translate-demo-container-computer"> 
-                    <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} width={16}>
+                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} id="translate-demo-container-computer"> 
+                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
                         <Responsive as={Grid} container>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
                                 <Header as="h1" id="translate-demo-header-computer">{this.renderTranslateHeader(this.props.user?.language)}</Header>
                             </Responsive>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
                                 <Header id="translate-demo-subheader-computer" as="h4">
                                 {this.renderTranslatePar(this.props.user?.language)}
                                 </Header>
                             </Responsive>
                         </Responsive>
                     </Responsive>
-                    <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Column} width={16}>
-                        <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid} container>
-                            <Responsive {...Responsive.onlyComputer} minWidth={800} maxWidth={1571} as={Grid.Row} centered>
+                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} container>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row} centered>
                                 <Image fluid id="translate-gif-computer" alt="translate-gif" src={AllChatDemo2} />
                             </Responsive>
                         </Responsive>
